24075e528ed226440f72085baaeec222e04aa59d
nicolasbilic <nicolasbilic9@gmail.com>
2024-12-27 18:14:35 +0100

    patch dropdown bug + close bookingwidget

--- src/components/TidyCalWidgetLegende.vue
+++ src/components/TidyCalWidgetLegende.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { onClickOutside } from '@vueuse/core';
+import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
 
 const showWidget = ref(false);
 
@@ -8,7 +8,11 @@ const loadWidget = () => {
   showWidget.value = true;
 
   // vérifie si le script TidyCal est déjà chargé
-  if (!document.querySelector('script[src="https://asset-tidycal.b-cdn.net/js/embed.js"]')) {
+  if (
+    !document.querySelector(
+      'script[src="https://asset-tidycal.b-cdn.net/js/embed.js"]'
+    )
+  ) {
     const script = document.createElement("script");
     script.src = "https://asset-tidycal.b-cdn.net/js/embed.js";
     script.async = true;
@@ -32,19 +36,35 @@ onClickOutside(tidyWidget, closeWidget);
   <div v-if="showWidget" class="overlay"></div>
 
   <!-- Bouton personnalisé -->
-  <button class="tidycal-button" @click="loadWidget">
-    Je réserve
-  </button>
+  <button class="tidycal-button" @click="loadWidget">Je réserve</button>
 
   <!-- Conteneur du widget -->
   <div v-if="showWidget" class="tidycal-widget-container" ref="tidyWidget">
-    <button class="close-button" @click="closeWidget" aria-label="Fermer le widget">
-      <svg class="close-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-hidden="true">
-        <path d="M18 6L6 18M6 6l12 12" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
-          fill="none" />
+    <button
+      class="close-button"
+      @click="closeWidget"
+      aria-label="Fermer le widget"
+    >
+      <svg
+        class="close-icon"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        aria-hidden="true"
+      >
+        <path
+          d="M18 6L6 18M6 6l12 12"
+          stroke="white"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          fill="none"
+        />
       </svg>
     </button>
-    <div class="tidycal-embed" data-path="nicolasbilicdev/forfait-legende"></div>
+    <div
+      class="tidycal-embed"
+      data-path="nicolasbilicdev/forfait-legende"
+    ></div>
   </div>
 </template>
 
@@ -89,7 +109,7 @@ onClickOutside(tidyWidget, closeWidget);
 
 .close-button {
   position: absolute;
-  top: 7px;
+  top: -50px;
   left: 40px;
   display: flex;
   justify-content: center;
@@ -98,7 +118,7 @@ onClickOutside(tidyWidget, closeWidget);
   width: 40px;
   height: 40px;
   color: var(--color-text-accent-light);
-  background: #3192F1;
+  background: #3192f1;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
   border-radius: 50%;
   cursor: pointer;
--- src/components/User/Dropdown.vue
+++ src/components/User/Dropdown.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-defineProps(['title', 'items']);
+defineProps(["title", "items"]);
 const isDropdownOpen = ref(false);
 let timeout: ReturnType<typeof setTimeout>;
 
@@ -15,7 +15,7 @@ const hideDropdown = () => {
     isDropdownOpen.value = false;
   }, 300);
 };
-const active = ref('active');
+const active = ref("active");
 </script>
 
 <template>
@@ -24,15 +24,25 @@ const active = ref('active');
       <a href="#">
         {{ title }}
       </a>
-      <svg width="7" height="4" viewBox="0 0 7 4" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path class="inner-path"
+      <svg
+        width="7"
+        height="4"
+        viewBox="0 0 7 4"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          class="inner-path"
           d="M6.40362 0.349316C6.28652 0.24067 6.12812 0.179688 5.963 0.179688C5.79788 0.179688 5.63948 0.24067 5.52237 0.349316L3.27862 2.41432L1.06612 0.349316C0.949022 0.24067 0.790615 0.179688 0.625499 0.179688C0.460383 0.179688 0.301975 0.24067 0.184874 0.349316C0.126294 0.403545 0.0797971 0.468062 0.0480667 0.539147C0.0163363 0.610231 0 0.686476 0 0.763483C0 0.84049 0.0163363 0.916735 0.0480667 0.98782C0.0797971 1.0589 0.126294 1.12342 0.184874 1.17765L2.83487 3.65098C2.89298 3.70566 2.9621 3.74905 3.03826 3.77867C3.11443 3.80828 3.19612 3.82353 3.27862 3.82353C3.36113 3.82353 3.44282 3.80828 3.51898 3.77867C3.59515 3.74905 3.66427 3.70566 3.72237 3.65098L6.40362 1.17765C6.4622 1.12342 6.5087 1.0589 6.54043 0.98782C6.57216 0.916735 6.5885 0.84049 6.5885 0.763483C6.5885 0.686476 6.57216 0.610231 6.54043 0.539147C6.5087 0.468062 6.4622 0.403545 6.40362 0.349316Z"
-          fill="white" />
+          fill="white"
+        />
       </svg>
       <transition name="fade" appear>
         <div class="sub-menu" v-if="isDropdownOpen">
           <div class="menu-item" v-for="(item, index) in items" :key="index">
-            <RouterLink :active-class="active" :to="item.link">{{ item.title }}</RouterLink>
+            <RouterLink :active-class="active" :to="item.link">{{
+              item.title
+            }}</RouterLink>
           </div>
         </div>
       </transition>
@@ -44,14 +54,13 @@ const active = ref('active');
 /****** Effet d'apparition du déroulant ******/
 .fade-enter-active,
 .fade-leave-active {
-  transition: all .3s ease-out;
+  transition: all 0.3s ease-out;
 }
 
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
 }
-
 
 /****** Global ******/
 .menu-wrapper {
@@ -71,14 +80,15 @@ nav .menu-item a:focus {
 }
 
 nav .menu-item a::after {
-  content: '';
+  content: "";
   position: absolute;
   top: 15px;
   left: 50%;
   transform: translateX(-50%) scale(0) rotateX(180deg);
   width: 90px;
   height: 30px;
-  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="white"/></svg>') no-repeat center;
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="white"/></svg>')
+    no-repeat center;
   background-size: contain;
   transition: transform 0.3s ease;
 }
@@ -87,28 +97,29 @@ nav .menu-item a:hover::after {
   transform: translateX(-50%) scale(0.6) rotateX(180deg);
 }
 
-
 /****** Active state ******/
 nav .menu-item a.active::after {
-  content: '';
+  content: "";
   position: absolute;
   top: 15px;
   left: 50%;
   width: 90px;
   height: 30px;
-  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="black"/></svg>') no-repeat center;
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="black"/></svg>')
+    no-repeat center;
   background-size: contain;
   transform: translateX(-50%) scale(0.6) rotateX(180deg);
 }
 
 nav .burger-menu .menu-item a.active::after {
-  content: '';
+  content: "";
   position: absolute;
   top: 15px;
   left: 50%;
   width: 90px;
   height: 30px;
-  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="black"/></svg>') no-repeat center;
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="black"/></svg>')
+    no-repeat center;
   background-size: contain;
   transform: translateX(-50%) scale(0) rotateX(180deg);
 }
@@ -134,14 +145,15 @@ nav .secondary-nav .menu-item a:focus {
 }
 
 nav .secondary-nav .menu-item a::after {
-  content: '';
+  content: "";
   position: absolute;
   top: 15px;
   left: 50%;
   transform: translateX(-50%) scale(0) rotateX(180deg);
   width: 90px;
   height: 30px;
-  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="black"/></svg>') no-repeat center;
+  background: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 30"><path d="M0 30 L5 10 L10 20 L15 5 L20 25 L25 15 L30 30 L35 10 L40 20 L45 5 L50 25 L55 15 L60 30 L65 10 L70 20 L75 5 L80 25 L85 15 L90 30 L95 10 L100 30 Z" fill="black"/></svg>')
+    no-repeat center;
   background-size: contain;
   transition: transform 0.3s ease;
 }
@@ -184,7 +196,6 @@ nav .menu-item .sub-menu .menu-item a {
   width: 100%;
   height: 100%;
   padding: 8px 20px;
-
 }
 
 nav .sub-menu .menu-item a:hover,
@@ -198,7 +209,7 @@ nav .sub-menu .menu-item a:hover::after {
 
 /******* Burger Menu *******/
 nav .burger-menu .menu-item:hover {
-  background-color: rgb(86, 127, 155)
+  background-color: rgb(86, 127, 155);
 }
 
 nav .menu-item a {
@@ -214,7 +225,7 @@ nav .burger-menu .menu-item a {
 }
 
 nav .burger-menu .menu-item a:hover {
-  background-color: rgb(86, 127, 155)
+  background-color: rgb(86, 127, 155);
 }
 
 nav .burger-menu .menu-item svg {
@@ -241,8 +252,8 @@ nav .burger-menu .menu-item a:focus {
 nav .burger-menu .menu-item .sub-menu {
   position: absolute;
   width: max-content;
-  top: 10px;
-  left: calc(50% + 160px);
+  top: 100%;
+  left: calc(50% + 0px);
   transform: translateX(-50%);
   text-align: center;
   padding: 0px;
@@ -254,5 +265,10 @@ nav .burger-menu .menu-item .sub-menu {
 
 nav .burger-menu .menu-item .sub-menu .menu-item a:hover {
   background-color: var(--color-dropdown-hover);
+}
+@media (width > 500px) {
+  nav .burger-menu .menu-item .sub-menu {
+    left: calc(50% + 160px);
+  }
 }
 </style>
