b71909309cc65c6139a862ad79b982ad0b3faa91
nicolasbilic <nicolasbilic9@gmail.com>
2025-05-25 15:40:05 +0200

    add comment

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,22 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-import UserFooter from './components/User/UserFooter.vue';
+import { ref, watch } from "vue";
+import { useRoute } from "vue-router";
+import UserFooter from "./components/User/UserFooter.vue";
 
 const route = useRoute();
-const AdminPage = ref(route.path.includes('/admin'));
-watch(() => route.path, (newPath) => {
-  AdminPage.value = newPath.includes('/admin');
-});
+const AdminPage = ref(route.path.includes("/admin"));
+watch(
+  () => route.path,
+  (newPath) => {
+    AdminPage.value = newPath.includes("/admin");
+  }
+);
 </script>
 
 <template>
   <div v-if="!AdminPage">
     <RouterView />
+    <!-- Footer -->
     <UserFooter />
   </div>
   <div v-if="AdminPage">
